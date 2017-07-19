94717cffb93c67db799cf5ab0969030b1c224375
DK\psk <psk@sitecore.net>
2017-07-19 15:57:53 -0400

    Initial fix

---
+++ src/Sitecore.Support.173890/ContentSearch/SolrProvider/Pipelines/FormatQueryFieldValue/ApplyFieldMappingRule.cs
@@ -0,0 +1,16 @@
+﻿namespace Sitecore.Support.ContentSearch.SolrProvider.Pipelines.FormatQueryFieldValue
+{
+    using Sitecore.ContentSearch.Pipelines.FormatQueryFieldValue;
+    public class ApplyFieldMappingRule : Sitecore.ContentSearch.SolrProvider.Pipelines.FormatQueryFieldValue.ApplyFieldMappingRule
+    {
+        protected override void FormatArgs(string schemaType, FormatQueryFieldValueArgs args)
+        {
+            if (schemaType.ToLower() == "datetime")
+            {
+                args.FieldValue = Escape(args.FieldValue, "", true, args.ExcludeEscapeCharacters);
+                return;
+            }
+            base.FormatArgs(schemaType, args);
+        }
+    }
+}
---
+++ src/Sitecore.Support.173890/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.173890")]
+[assembly: AssemblyProduct("Sitecore.Support.173890")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Solr search provider fails to compare DateTime fields for equality")]
+
